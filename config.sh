e3a30644927ef51adb46ea536e1b99eaad74448a
Matthew Brett <matthew.brett@gmail.com>
2016-09-10 16:21:01 -0700

    Fix up test command

--- config.sh
+++ config.sh
@@ -10,5 +10,5 @@ function pre_build {
 function run_tests {
     # Runs tests on installed distribution from an empty directory
     python --version
-    nosetests ../pywt/tests
+    nosetests ../pywt/pywt/tests
 }
